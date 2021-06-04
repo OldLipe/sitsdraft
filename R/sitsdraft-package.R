183ec4d9dbf2747f9186e74c3c5d30fdb0e3c968
Felipe Carvalho <lipecaso@gmail.com>
2021-06-04 16:40:44 +0000

    update documentation

---
+++ R/sitsdraft-package.R
@@ -0,0 +1,14 @@
+#' @title Provides a basic template for applications based on SITS package and
+#' reproducible research tools.
+#'
+#' @name sitsdraft
+"_PACKAGE"
+NULL
+
+#' @importFrom sits sits_rfor sits_get_data sits_metadata_to_csv sits_train sits_timeline sits_classify
+#' @importFrom usethis ui_done
+#' @importFrom rrtools use_compendium use_readme_rmd
+#' @importFrom dplyr arrange bind_rows
+#' @importFrom furrr future_map
+#' @importFrom future plan
+NULL
